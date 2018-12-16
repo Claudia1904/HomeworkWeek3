ff0355b35cfc3551c46b8445127f8b1d6764141f
Claudia <lazaroiu.claudia04@gmail.com>
2018-12-16 15:00:06 +0200

    Add project files.

---
+++ HomeworkWeek3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkWeek3
+{
+    class Program
+    {
+        public enum Gender
+        {
+            MALE,
+            FEMALE
+        }
+
+        public static void DateOfBirthAndAge()
+        {
+            //Date of birth
+            Console.WriteLine("Enter your day of birth: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your month of birth (1-12): ");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your year of birth: ");
+            int year = int.Parse(Console.ReadLine());
+            DateTime dateOfBirth = new DateTime(year, month, day);
+
+            //Age
+            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
+            double ageInDays = timeSpan.TotalDays;
+            double actualAge = ageInDays / 365;
+        }
+
+        static void Main(string[] args)
+        {
+            DateOfBirthAndAge();
+
+            //Enter the gender
+            Console.WriteLine("Enter gender (m/f): ");
+            string genderLetter = Console.ReadLine().ToLower().Trim();
+            Gender gender = Gender.MALE;
+            switch (genderLetter)
+            {
+                case "m":
+                    gender = Gender.MALE;
+                    break;
+                case "f":
+                    gender = Gender.FEMALE;
+                    break;
+                default:
+                    throw new Exception("Invalid value for gender");
+            }
+
+            //Age of retirement
+            int ageOfRetirement = (gender == Gender.MALE) ? 65 : 63;
+            Console.WriteLine(ageOfRetirement);
+
+        }
+    }
+}
---
+++ StringBuildIn/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuildIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Compare strings
+            string name = "Claudia";
+            string secondName = "Claudia";
+            int c = string.Compare(name, secondName);
+            Console.WriteLine(c);
+
+            //Concatenate strings
+            string one = "Hello";
+            string two = ", ";
+            string three = "World";
+            string four = "!";
+            string concatenare = string.Concat(one, two, three, four);
+            Console.WriteLine(concatenare);
+
+            //Empty string
+            string empty = string.Empty;
+
+            //Check if string in null or empty
+            string str = null;
+            bool checkString = string.IsNullOrEmpty(str);
+            Console.WriteLine(checkString);
+
+            //Format string
+            string day = "12";
+            string month = "12";
+            string year = "2018";
+            string result = string.Format("{0}/{1}/{2:yyyy}", day, month, year);
+            Console.WriteLine(result);
+
+            //Start with, End with
+            string str3 = "I need more sleep";
+            bool testString = str3.StartsWith("more");
+            bool testString2 = str3.EndsWith("ep");
+            Console.WriteLine(testString);
+            Console.WriteLine(testString2);
+
+
+            //Contain substring
+            string str2 = "Pentastagiu week 3";
+            bool testSubstring = str2.Contains("Penta");
+            Console.WriteLine(testSubstring);
+
+
+
+        }
+    }
+}
